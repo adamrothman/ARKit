c5e01a27a2099ab42f1ae2d08e7abc861232517c
Adam Rothman <rothman.adam@gmail.com>
2012-11-19 12:52:39 -0800

    Add clearer instructions

--- Views/ARTextView.h
+++ Views/ARTextView.h
@@ -6,6 +6,10 @@
 //
 //  A UITextView that supports placeholder text.
 //
+//  You must configure the text view's delegate so that textViewDidChange:
+//  calls setNeedsDisplay on the text view (to make the placeholder disappear
+//  or reappear as appropriate).
+//
 
 #import <UIKit/UIKit.h>
 
